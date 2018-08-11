6ba84822b76a344b73da74f41183a3e0679509b1
ogawa-takuma <tkm.op119@gmail.com>
2018-08-11 11:53:32 +0900

    FizzBazz test

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	for i = 0; i < 100; i++ {
+		if (i  == 0){
+			fmt.Printf("%d\n", i)
+		} else if (i % 3 == 0 && i % 5 == 0){
+			fmt.Println("fizzbazz")
+		} else if (i % 3 == 0){
+			fmt.Println("fizz")
+		} else if (i % 5 == 0){
+			fmt.Println("bazz")
+		} else {
+			fmt.Printf("%d\n", i)
+		}
+	}
+}
